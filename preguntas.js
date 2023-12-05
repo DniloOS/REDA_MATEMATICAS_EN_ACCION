f1f7e9d88481988fb6bbeebf782b587aca22c67a
DniloOS <143416671+DniloOS@users.noreply.github.com>
2023-12-05 18:18:59 -0500

    NUEVO TRABAJO
    ACOMODEN BIEN LAS PAGUINAS

--- preguntas.js
+++ preguntas.js
@@ -1,16 +1,35 @@
 function calificar() {
-    let calificacion = 0; 
-    let respuesta1 = document.getElementById("pregunta1").value;
-    let respuesta2 = document.getElementById("pregunta2").value;
-    let respuesta3 = document.getElementById("pregunta3").value;
-    if (respuesta1 == "6") {
-        calificacion++;
+    let calificacion = 0;
+    const respuestasCorrectas = {
+        pregunta1: "6",
+        pregunta2: "11",
+        pregunta3: "8"
+    };
+
+    const obtenerRespuesta = (id) => document.getElementById(id).value;
+
+    for (const pregunta in respuestasCorrectas) {
+        const respuesta = obtenerRespuesta(pregunta);
+        if (respuesta === respuestasCorrectas[pregunta]) {
+            calificacion++;
+        }
     }
-    if (respuesta2 == "11") {
-        calificacion++;
+
+    mostrarResultado(calificacion);
+}
+
+function mostrarResultado(calificacion) {
+    const mensaje = `Tu nota es: ${calificacion}`;
+    const resultadoElemento = document.getElementById("resultado");
+    
+    if (resultadoElemento) {
+        resultadoElemento.textContent = mensaje;
+    } else {
+        const resultadoDiv = document.createElement("div");
+        resultadoDiv.id = "resultado";
+        resultadoDiv.textContent = mensaje;
+
+        const formulario = document.querySelector("form");
+        formulario.insertAdjacentElement("afterend", resultadoDiv);
     }
-    if (respuesta3 == "8") {
-        calificacion++;
-    }
-    alert("Tu nota es: " + calificacion);
 }
